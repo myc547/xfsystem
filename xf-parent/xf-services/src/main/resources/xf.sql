7a0b8f151ce609ef429e1484fcfaa65326443e2b
xp547@126.com <mayongchang123>
2017-12-11 23:22:53 +0800

    视频管理模块

--- xf-parent/xf-services/src/main/resources/xf.sql
+++ xf-parent/xf-services/src/main/resources/xf.sql
@@ -44,5 +44,64 @@ CREATE TABLE  `t_course` (
   `course_status` INT(1) COMMENT '课程状态,0:新建,1:提交(待审批),2:审批不通过,3:审批通过,4:发布',
   `course_free` INT(1) COMMENT '课程是否收付费,0:免费,1:收费',
   `cover_path` VARCHAR(200) COMMENT '封面图片路径',
+  `creator_id` VARCHAR(32) COMMENT '创建人id',
+  `create_time` TIMESTAMP COMMENT '创建时间',
+  `editor_id` VARCHAR(32) COMMENT '修改人id',
+  `edit_time` TIMESTAMP  NULL ON UPDATE  current_timestamp COMMENT '编辑时间',
   PRIMARY KEY (`id`)
 )COMMENT '课程表';
+
+-- 审核表
+CREATE TABLE `t_audits` (
+  `id` VARCHAR(32) NO NULL COMMIT '主键',
+  `business_id` VARCHAR(32) NOT NULL COMMIT '业务id,审核对象的业务',
+  `auditor_id` VARCHAR(32) COMMIT '审核人id',
+  `audit_time` TIMESTAMP DEFAULT current_timestamp COMMIT '审核时间',
+  `audit_recommend` VARCHAR(500) COMMIT '审核建议',
+  PRIMARY KEY (`id`),
+  KEY `index_business_id` (`business_id`)
+) COMMIT '审核表';
+
+-- 课程详情表
+CREATE TABLE  `t_course_detail` (
+  `id` VARCHAR(32) NOT NULL COMMENT '主键',
+  `course_id` VARCHAR(32) COMMENT '课程表id',
+  `detail_desc` TEXT COMMENT '详情描述',
+  `creator_id` VARCHAR(32) COMMENT '创建人id',
+  `create_time` TIMESTAMP COMMENT '创建时间',
+  `editor_id` VARCHAR(32) COMMENT '修改人id',
+  `edit_time` TIMESTAMP  NULL ON UPDATE  current_timestamp COMMENT '编辑时间',
+  PRIMARY KEY (`id`),
+  KEY `index_course_id` (`course_id`)
+)COMMENT '课程详情表';
+
+-- 课程目录表
+CREATE TABLE  `t_course_catalog` (
+  `id` VARCHAR(32) NOT NULL COMMENT '主键',
+  `pid` VARCHAR(32) NOT NULL COMMENT '父级主键',
+  `course_id` VARCHAR(32) COMMENT '课程表id',
+  `media_id` VARCHAR(32) COMMENT '视频id',
+  `catalog_title` VARCHAR(200) COMMENT '目录说明',
+  `creator_id` VARCHAR(32) COMMENT '创建人id',
+  `create_time` TIMESTAMP COMMENT '创建时间',
+  `editor_id` VARCHAR(32) COMMENT '修改人id',
+  `edit_time` TIMESTAMP  NULL ON UPDATE  current_timestamp COMMENT '编辑时间',
+  PRIMARY KEY (`id`),
+  KEY `index_pid` (`pid`),
+  KEY `index_course_id` (`course_id`),
+  KEY `index_media_id` (`media_id`)
+)COMMENT '课程目录表';
+
+-- 课程评论表
+CREATE TABLE  `t_course_reviews` (
+  `id` VARCHAR(32) NOT NULL COMMENT '主键',
+  `course_id` VARCHAR(32) COMMENT '课程表id',
+  `review_desc` VARCHAR(400) COMMENT '评论信息',
+  `reviewer_id` VARCHAR(32) COMMENT '评论人id',
+  `reviewer_time` TIMESTAMP COMMENT '评论时间',
+  PRIMARY KEY (`id`),
+  KEY `index_course_id` (`course_id`),
+  KEY `index_reviewer_id` (`reviewer_id`)
+)COMMENT '课程评论表';
+
+
